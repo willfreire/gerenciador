4913478d3b56b786a3378732a77041a228f4943f
William Freire Alves <willfreal@gmail.com>
2018-03-01 18:38:04 -0300

    Alteração da funçao de validação de e-mail

--- system/libraries/Email.php
+++ system/libraries/Email.php
@@ -1030,7 +1030,7 @@ class CI_Email {
 	 * @param	string
 	 * @return	bool
 	 */
-	public function valid_email($email)
+	/* public function valid_email($email)
 	{
 		if (function_exists('idn_to_ascii') && strpos($email, '@'))
 		{
@@ -1042,9 +1042,20 @@ class CI_Email {
 		}
 
 		return (bool) filter_var($email, FILTER_VALIDATE_EMAIL);
-	}
+	} */
+        
+        public function valid_email($str)
+        {
+            $matches = array();
+            if (function_exists('idn_to_ascii') && preg_match('#\A([^@]+)@(.+)\z#', $str, $matches))
+            {
+                $variant = defined('INTL_IDNA_VARIANT_UTS46') ? INTL_IDNA_VARIANT_UTS46 : INTL_IDNA_VARIANT_2003;
+                $str = $matches[1] . '@' . idn_to_ascii($matches[2], 0, $variant);
+            }
+            return (bool) filter_var($str, FILTER_VALIDATE_EMAIL);
+        }
 
-	// --------------------------------------------------------------------
+    // --------------------------------------------------------------------
 
 	/**
 	 * Clean Extended Email Address: Joe Smith <joe@smith.com>
